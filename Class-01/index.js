e5cd38f517a46249437fcc387f4d4b601299cf23
Abdul Hannan <abdulhannanhere@gmail.com>
2025-06-29 15:19:04 -0700

    postman and requests

--- Class-01/index.js
+++ Class-01/index.js
@@ -4,8 +4,18 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Get request called!");
 });
+
+app.post("/", (req, res) => {
+    res.send("Post request called!");
+})
+app.put("/", (req, res) => {
+    res.send("Put request called!");
+})
+app.delete("/", (req, res) => {
+    res.send("Delete request called!");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
