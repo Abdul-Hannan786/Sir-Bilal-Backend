da06c654f44af09dd02b4021cf3716b37005ecd0
Abdul Hannan <abdulhannanhere@gmail.com>
2025-06-29 15:49:01 -0700

    Middlewares learned

--- Class-01/index.js
+++ Class-01/index.js
@@ -1,21 +1,34 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
+
+// {Application Level Middleware}
+const middleware = (req, res, next) => {
+  console.log("Middleware called", Date.now());
+  // res.status(500).send("Internal Server Error");
+  next();
+};
+
+app.use(middleware);
+app.use(morgan("tiny"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Get request called!");
 });
 
 app.post("/", (req, res) => {
-    res.send("Post request called!");
-})
+  console.log(req.body)
+  res.send("Post request called!");
+});
 app.put("/", (req, res) => {
-    res.send("Put request called!");
-})
+  res.send("Put request called!");
+});
 app.delete("/", (req, res) => {
-    res.send("Delete request called!");
-})
+  res.send("Delete request called!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
