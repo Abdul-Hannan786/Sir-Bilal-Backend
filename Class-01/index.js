3c3891ad6867dae90e1d1a8c7bf9c7ac582b58a3
Abdul Hannan <abdulhannanhere@gmail.com>
2025-07-01 05:14:23 +0500

    params and query in backend

--- Class-01/index.js
+++ Class-01/index.js
@@ -16,8 +16,37 @@ app.use(middleware);
 app.use(morgan("tiny"));
 app.use(express.json());
 
+const tasks = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+  { id: 3, title: "Task 3", completed: false },
+];
+
+// {Params}
 app.get("/", (req, res) => {
-  res.send("Get request called!");
+  res.status(200).send(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  const task = tasks.find((task) => task.id == id);
+  if (task) {
+    res.status(200).send(task);
+  } else {
+    res.status(404).send({ message: "Task not found" });
+  }
+});
+// {query}
+
+app.get("/tasks", (req, res) => {
+  const { completed } = req.query;
+  let filter = tasks;
+  if (completed) {
+    filter = tasks.filter((data) =>
+      completed === "true" ? data.completed === true : data.completed === false
+    );
+  }
+  res.status(200).send(filter);
 });
 
 // app.post("/", (req, res) => {
@@ -31,7 +60,7 @@ app.get("/", (req, res) => {
 //   res.send("Delete request called!");
 // });
 
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
